a4b1abb16dc1830ed30a23ef243ca3f97d3a0983
Luis Ramos <lm.solares@ciencias.unam.mx>
2026-09-01 14:24:29 -0600

    Agregar script de la vista de la camara

---
+++ Laboratorio/Practica01_3D/Practica01_3D/Assets/Scripts/CameraLook.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour {
+  public float sensitivity = 200f;
+  float xRotation = 0f;
+  
+  void Update(){
+    float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+    float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+    xRotation -= mouseY;
+    xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+    transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    transform.parent.Rotate(Vector3.up * mouseX);
+  }
+}
