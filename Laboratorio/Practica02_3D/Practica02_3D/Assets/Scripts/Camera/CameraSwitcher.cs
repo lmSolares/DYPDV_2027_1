911a87410bc0c8ca7e975cc9f5b66857850266f7
Luis Ramos <lmsr.solares@gmail.com>
2026-09-20 14:58:07 -0600

    practica02-3D: preparación de la practica

---
+++ Laboratorio/Practica02_3D/Practica02_3D/Assets/Scripts/Camera/CameraSwitcher.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour {
+    public GameObject fpsCam;
+    public GameObject tpsCam;
+    bool usingFPS = false;
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Tab)){
+            usingFPS = !usingFPS;
+            fpsCam.SetActive(usingFPS);
+            tpsCam.SetActive(!usingFPS);
+        }
+    }
+}
