6cb744848f74fd746785cef45dd21bf552d653d8
Luis Ramos <lmsr.solares@gmail.com>
2026-09-20 14:55:12 -0600

    practica013D: se reparo la practica, ya que esta no se subio completa por problemas en el gitignore

--- Laboratorio/Practica01_3D/Practica01_3D/Assets/Scripts/Camera/CameraSwitcher.cs
+++ Laboratorio/Practica01_3D/Practica01_3D/Assets/Scripts/Camera/CameraSwitcher.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 
 public class CameraSwitcher : MonoBehaviour {
-  public GameObject fpsCam;
-  public GameObject tpsCam;
-  bool usingFPS = false;
+    public GameObject fpsCam;
+    public GameObject tpsCam;
+    bool usingFPS = false;
 
-  void Update(){
-    if(Input.GetKeyDown(KeyCode.Tab)){
-      usingFPS = !usingFPS;
-      fpsCam.SetActive(usingFPS);
-      tpsCam.SetActive(!usingFPS);
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Tab)){
+            usingFPS = !usingFPS;
+            fpsCam.SetActive(usingFPS);
+            tpsCam.SetActive(!usingFPS);
+        }
     }
-
-  }
 }
--- Laboratorio/Practica01_3D/Practica01_3D/Assets/Scripts/PlayerMovement.cs
+++ Laboratorio/Practica01_3D/Practica01_3D/Assets/Scripts/PlayerMovement.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     public float speed = 6f;
     public float runSpeed = 10f;
+
     public float gravity = -9.8f;
     public float jumpForce = 8f;
+
     CharacterController controller;
     Vector3 velocity;
+
     void Start(){
         controller = GetComponent<CharacterController>();
     }
+
     void Update(){
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -16,10 +20,9 @@ public class PlayerMovement : MonoBehaviour {
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
         float currentSpeed = isRunning ? runSpeed : speed;
         controller.Move(move * currentSpeed * Time.deltaTime);
-        if(controller.isGrounded && velocity.y < 0)
-            velocity.y = -2f;
-        if(Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
-            velocity.y = jumpForce;
+
+        if(controller.isGrounded && velocity.y < 0) velocity.y = -2f;
+        if(Input.GetKeyDown(KeyCode.Space) && controller.isGrounded) velocity.y = jumpForce;
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
